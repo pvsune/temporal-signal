5e0ff941407d14a131a5697b0f042990324492e7
pvsune <pvsune@hotmail.com>
2022-04-27 20:02:09 +0800

    Add helloworld app.

---
+++ worker/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"log"
+
+	"go.temporal.io/sdk/client"
+	"go.temporal.io/sdk/worker"
+
+	ts "github.com/pvsune/signal"
+)
+
+func main() {
+	// The client and worker are heavyweight objects that should be created once per process.
+	c, err := client.NewClient(client.Options{})
+	if err != nil {
+		log.Fatalln("Unable to create client", err)
+	}
+	defer c.Close()
+
+	w := worker.New(c, "temporal-signal", worker.Options{})
+
+	w.RegisterWorkflow(ts.Workflow)
+	w.RegisterActivity(ts.Activity)
+
+	err = w.Run(worker.InterruptCh())
+	if err != nil {
+		log.Fatalln("Unable to start worker", err)
+	}
+}
