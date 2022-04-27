f195b6d722ba0189a542780231cb0da26d6bc0d9
pvsune <pvsune@hotmail.com>
2022-04-28 00:00:47 +0800

    Add signal to extend timer.

--- signal.go
+++ signal.go
@@ -19,25 +19,13 @@ func Workflow(ctx workflow.Context, name string) (string, error) {
 	logger := workflow.GetLogger(ctx)
 	logger.Info("Signal workflow started", "name", name)
 
-	var signalVal string
-	signalChan := workflow.GetSignalChannel(ctx, "your-signal-name")
-	selector := workflow.NewSelector(ctx)
-	selector.AddReceive(signalChan, func(channel workflow.ReceiveChannel, more bool) {
-		channel.Receive(ctx, &signalVal)
-	})
-	timerFuture := workflow.NewTimer(ctx, time.Second*10)
-	selector.AddFuture(timerFuture, func(future workflow.Future) {
-		signalVal = "World"
-	})
-	selector.Select(ctx)
-
-	switch signalVal {
-	case "cancel":
-		return "", temporal.NewCanceledError()
+	signalVal, err := waitForSignal(ctx)
+	if err != nil {
+		return "", err
 	}
 
 	var result string
-	err := workflow.ExecuteActivity(ctx, Activity, signalVal).Get(ctx, &result)
+	err = workflow.ExecuteActivity(ctx, Activity, signalVal).Get(ctx, &result)
 	if err != nil {
 		logger.Error("Activity failed.", "Error", err)
 		return "", err
@@ -52,4 +40,26 @@ func Activity(ctx context.Context, name string) (string, error) {
 	logger := activity.GetLogger(ctx)
 	logger.Info("Activity", "name", name)
 	return "Hello " + name + "!", nil
+}
+
+func waitForSignal(ctx workflow.Context) (string, error) {
+	var signalVal string = "World"
+	signalChan := workflow.GetSignalChannel(ctx, "your-signal-name")
+	selector := workflow.NewSelector(ctx)
+	selector.AddReceive(signalChan, func(channel workflow.ReceiveChannel, more bool) {
+		channel.Receive(ctx, &signalVal)
+	})
+	timerFuture := workflow.NewTimer(ctx, time.Second*10)
+	selector.AddFuture(timerFuture, func(future workflow.Future) {
+		// Do nothing.
+	})
+	selector.Select(ctx)
+
+	switch signalVal {
+	case "cancel":
+		return "", temporal.NewCanceledError()
+	case "extend":
+		return waitForSignal(ctx)
+	}
+	return signalVal, nil
 }
